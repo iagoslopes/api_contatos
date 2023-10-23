00cbb82d5a16be71fa34bb1612c6d847a160a4dd
Iago de Souza Lopes <128859658+iagoslopes@users.noreply.github.com>
2023-10-23 20:25:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.json());
 const contatoRouter = require('./routes/contatoRoutes');
 app.use('/contatos', contatoRouter);
 
+app.use((req, res, next)=>{
+  res.status(404).json({"message":"rota não definida"})
+})
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,3 +28,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+module.exports = app
